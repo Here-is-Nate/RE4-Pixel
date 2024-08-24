f1aeafcce418b06ecbfcf84e25bbd10fb3e19705
Here-is-Nate <brasilinonatan@gmail.com>
2024-08-24 19:36:50 -0300

    Shake and Random Point
    Addeded a shake value to laser aim, and a random point inside the crosshair

--- Assets/Scripts/Guns/GunAim.cs
+++ Assets/Scripts/Guns/GunAim.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GunAim : MonoBehaviour
@@ -19,6 +15,7 @@ public class GunAim : MonoBehaviour
     }
     [SerializeField] private EAimType aimType;
     private float maxShakeValue = 1.8f;
+    private float shakeValueLimiter = 1.2f;
     private float shakeValue;
     private GameObject _aimedGO;
     private bool _isAiming;
@@ -27,11 +24,18 @@ public class GunAim : MonoBehaviour
     [SerializeField] private GameObject crossHairRender;
     [SerializeField] private Transform crossHairCenter;
     [SerializeField] private GameObject[] crossHairSides;
+    [SerializeField] private GameObject crossHairHitPoint;
     private float minCrossHairDistance = 0.1f;
 
     [Header("Laser Config")]
     [SerializeField] private GameObject laserPoint;
+    [SerializeField] private bool shakeLaser;
     private float laserDistance = 10f;
+    private float xPosition;
+    private float yPosition;
+    private float laserShakeDrag = 0.04f;
+    private float laserShakeDragCount;
+    private float laserShakeInterval = 0.05f;
 
     #region Properties
     public GameObject aimedGO {get {return _aimedGO;}}
@@ -71,48 +75,72 @@ public class GunAim : MonoBehaviour
         DeactiveLaserPoint();
         DeactiveCrossHair();
 
+        _aimedGO = null;
+
         Cursor.visible = true;
     }
 
     void OnAim() {
-        float direction = playerAim.GetAimAngle();
-
         Cursor.visible = false;
 
-        // Convert a degree angle to radians
-        float dirRadians = direction * Mathf.Deg2Rad;
-
-        float xPosition = Mathf.Sin(dirRadians);
-        float yPosition = Mathf.Cos(dirRadians);
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xPosition, yPosition), laserDistance, layerMask);
-
-        if(hit) _aimedGO = hit.transform.gameObject;
-        else _aimedGO = null;
-
         switch(aimType) {
-            case EAimType.LASER: LaserAim(hit, xPosition, yPosition); break;
+            case EAimType.LASER: LaserAim(); break;
             case EAimType.CROSSHAIR: CrossHairAim(); break;
-        }
-        
+        }  
     }
 
     public GameObject GetAimedGameObject() {
+        switch(aimType) {
+            case EAimType.LASER: return aimedGO;
+            case EAimType.CROSSHAIR: return GetCrossHairHitPointGO();
+        }
+
         return aimedGO;
     }
 
     #region Laser Handler
-    void LaserAim(RaycastHit2D hit, float xPosition, float yPosition) {
+    void LaserAim() {
+        float direction = playerAim.GetAimAngle();
+
+        // Convert a degree angle to radians
+        float dirRadians = direction * Mathf.Deg2Rad;
+
+        if(!shakeLaser) {
+            xPosition = Mathf.Sin(dirRadians);
+            yPosition = Mathf.Cos(dirRadians);
+        }
+
+        if(shakeLaser) {
+            laserShakeDragCount += Time.deltaTime;
+    
+            if(laserShakeDragCount >= laserShakeDrag) {
+                laserShakeDragCount = 0;
+
+                float crossDistance;
+
+                if(shakeValue > shakeValueLimiter) crossDistance = laserShakeInterval;
+                else crossDistance = shakeValue / shakeValueLimiter * laserShakeInterval;
+
+                xPosition = Mathf.Sin(dirRadians) + Random.Range(-crossDistance, crossDistance);
+                yPosition = Mathf.Cos(dirRadians) + Random.Range(-crossDistance, crossDistance);
+            }
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xPosition, yPosition), laserDistance, layerMask);
+
         if(hit) {
             DrawLaser(laserRenderer.transform.position, hit.point);
 
             laserPoint.SetActive(true);
             laserPoint.transform.position = hit.point;
             
+            _aimedGO = hit.transform.gameObject;
         } else {
             DrawLaser(laserRenderer.transform.position, new Vector2(laserRenderer.transform.position.x + xPosition * laserDistance, laserRenderer.transform.position.y + yPosition * laserDistance));
 
             DeactiveLaserPoint();
+
+            _aimedGO = null;
         }
     }
 
@@ -138,10 +166,20 @@ public class GunAim : MonoBehaviour
         crossHairRender.SetActive(true);
 
         float crossDistance;
+
         if(shakeValue > 1.2f) crossDistance = 0.3f;
         else crossDistance = shakeValue / 1.2f * 0.3f;
 
         DrawCrossHair(Camera.main.ScreenToWorldPoint(Input.mousePosition), crossDistance);
+
+        // Get a Random Point Inside the Crosshair
+        float hitPointX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        float hitPointY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+        hitPointX += Random.Range(-crossDistance, crossDistance);
+        hitPointY += Random.Range(-crossDistance, crossDistance);
+
+        crossHairHitPoint.transform.position = new Vector2(hitPointX, hitPointY);
     }
 
     void DrawCrossHair(Vector2 position, float crossDistance) {
@@ -149,14 +187,23 @@ public class GunAim : MonoBehaviour
 
         // Create a distance between cross sides
         crossHairSides[0].transform.position = new Vector2(crossHairRender.transform.position.x, crossHairRender.transform.position.y + crossDistance + minCrossHairDistance);
+
         crossHairSides[1].transform.position = new Vector2(crossHairRender.transform.position.x + crossDistance + minCrossHairDistance, crossHairRender.transform.position.y);
+
         crossHairSides[2].transform.position = new Vector2(crossHairRender.transform.position.x, crossHairRender.transform.position.y - (crossDistance + minCrossHairDistance));
+
         crossHairSides[3].transform.position = new Vector2(crossHairRender.transform.position.x - (crossDistance + minCrossHairDistance), crossHairRender.transform.position.y);
 
     }
 
     void DeactiveCrossHair() {
         if(crossHairRender.activeInHierarchy) crossHairRender.SetActive(false);
+    }
+
+    GameObject GetCrossHairHitPointGO() {
+        Collider2D collider = Physics2D.OverlapPoint(crossHairHitPoint.transform.position);
+
+        return collider != null ? collider.gameObject : null;
     }
     #endregion
 }
